695c1ba39eb37a218c8e556afb6263fde62921cb
Emil Brandon Kunju Herrera <buyme3409@gmail.com>
2026-03-11 09:28:17 +0800

    updated todo list

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import ErrorPage from "./pages/ErrorPage";
 import AddTodo from "./pages/AddTodo";
 import useLocalStorage from "use-local-storage";
 import { TodoContext } from "./contexts/TodoContext";
+import EditTodo from "./pages/EditTodo";
 
 
 function Layout() {
@@ -34,6 +35,7 @@ export default function App() {
             <Route index element={<Home />} />
             <Route path="add" element={<AddTodo />} />
             <Route path="*" element={<ErrorPage />} />
+            <Route path="todo/:id" element={<EditTodo />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/pages/EditTodo.jsx
@@ -0,0 +1,51 @@
+import { useContext, useState } from "react";
+import { Button } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import { TodoContext } from "../contexts/TodoContext";
+import { useNavigate, useParams } from "react-router-dom";
+
+export default function EditTodo() {
+    const setTodos = useContext(TodoContext).setTodos;
+    const todos = useContext(TodoContext).todos;
+    const navigate = useNavigate();
+    const id = parseInt(useParams().id);
+    //Therefore, we use the useParams hook to get the id from the route.
+
+    const currentTodo = todos.filter((t) => t.id === id)[0];
+    const [title, setTitle] = useState(currentTodo.title);
+    const [description, setDescription] = useState(currentTodo.description);
+    const [completed, setCompleted] = useState(currentTodo.completed);
+
+
+    function updateTodo(event) {
+        event.preventDefault();
+        const updatedTodos = todos.map((todo) => {
+            if (todo.id === id) {
+                return { id, title, description, completed };
+            }
+            return todo;
+        });
+        setTodos(updatedTodos);
+        navigate("/");
+    }
+
+    return (
+        <Container>
+            <h1 className="my-3">Add Todo</h1>
+            <Form onSubmit={updateTodo}>
+                <Form.Group className="mb-3" controlId="formTitle">
+                    <Form.Label>Title</Form.Label>
+                    <Form.Control type="text" placeholder="Get software developer job" value={title} onChange={(e) => setTitle(e.target.value)} required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formDescription">
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control as="textarea" rows={3} placeholder={`1. Create amazing project\n2. Apply to Google & Netflix\n3. Crush interview`} value={description} onChange={(e) => setDescription(e.target.value)} required />
+                </Form.Group>
+                <Form.Check type="checkbox" id="completed" label="Mark as completed" checked={completed} onChange={(e) => setCompleted(e.target.checked)} className="mb-3" />
+                <Button variant="primary" type="submit">
+                    Submit</Button>
+            </Form>
+        </Container>
+    );
+}
